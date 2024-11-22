b1c6600843de70c644a65717c1259a428fbd7bb7
Iva Papukchieva <ivapapukchieva28@gmail.com>
2024-11-22 21:01:36 +0100

    Filters adapted for the GUI

--- src/main/java/com/example/project11/ProjectInfo/Filters/FilterBySpecificGrade.java
+++ src/main/java/com/example/project11/ProjectInfo/Filters/FilterBySpecificGrade.java
@@ -1,15 +1,34 @@
 package com.example.project11.ProjectInfo.Filters;
 
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class FilterBySpecificGrade {
+    private double[][] allStudents;
+    private int grade;
 
-    public void FilterByGrade(int grade) {
+    public void FilterByGrade(int grade, double[][] allStudents) {
+        this.grade = grade;
+        this.allStudents = allStudents;
 
 
+    }
 
+    public double[][] FilterByGrade(){
 
+        double[][] filteredStudents = new double[allStudents.length][allStudents[0].length];
+        for (int i = 0; i < allStudents.length; i++) {
+            for (int j = 0; j < allStudents[i].length; j++) {
+                if (allStudents[i][j] != grade) {
+                    filteredStudents[i][j] = -1;
+                } else {
+                    filteredStudents[i][j] = allStudents[i][j];
+                }
+            }
+        }
 
-
+        return filteredStudents;
     }
 
 
