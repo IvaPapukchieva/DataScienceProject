f0a4f75e5bdcf3d9976539a6264b048396dde8cb
Vargas Garcia, Marcos (Stud. DACS) <m.vargasgarcia@student.maastrichtuniversity.nl>
2024-11-25 08:39:00 +0000

    Upload New File

---
+++ src/main/java/com/example/project11/ProjectInfo/STEP4/NormalDis.java
@@ -0,0 +1,28 @@
+package Step4;
+
+public class NormalDis {
+
+    private double gradesArray[] ; 
+    private double distribution[] = new double[11] ; 
+
+    public NormalDis(double[] gradesArray){
+        this.gradesArray = gradesArray ;
+
+    }
+    public double[] getNormalDistribution(){
+        calculateNoramlDistribution() ; 
+        return distribution; 
+    }
+    public void calculateNoramlDistribution(){
+        for (int i = 0 ; i<gradesArray.length ; i++){
+            for (int j = 0 ; j<distribution.length ; j++){
+                if( gradesArray[i] == j){
+                    distribution[j]++;
+                }
+
+            }
+        }
+    }
+    
+    
+}
