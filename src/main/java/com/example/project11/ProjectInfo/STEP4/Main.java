1c6dc7567726c456713528c4d2d61a23cd89f0f5
Vargas Garcia, Marcos (Stud. DACS) <m.vargasgarcia@student.maastrichtuniversity.nl>
2024-11-25 08:38:50 +0000

    Upload New File

---
+++ src/main/java/com/example/project11/ProjectInfo/STEP4/Main.java
@@ -0,0 +1,17 @@
+package Step4;
+
+import java.util.Arrays;
+
+class Main{
+        public static void main(String[] args) throws Exception{
+            // make it more efficient by checking the state of the course and if it is the same i do not have to invoke certain methods 
+            // else if it is different i need to invoke those methods, but at least it works :)
+
+            prediction effPrediction = new prediction(0 , " ");
+     
+
+            System.out.println(Arrays.deepToString(effPrediction.getPrediction()));
+
+            
+        }
+    }
