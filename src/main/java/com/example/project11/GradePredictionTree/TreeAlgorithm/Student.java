aa67ce2d6cdffbfb3b2601cd610e41a4c9081761
Raul <r.parau@student.maastrichtuniversity.nl>
2025-01-08 12:42:30 +0100

    Area Chart and Bubble Chart with fxml files

--- src/main/java/com/example/project11/GradePredictionTree/TreeAlgorithm/Student.java
+++ src/main/java/com/example/project11/GradePredictionTree/TreeAlgorithm/Student.java
@@ -12,36 +12,35 @@ public class Student {
     //sets Category 1 to a random value
     public String Category1RandomValue(String[][]CategoryArray){
         Random random = new Random();
-        int randomNumber = random.nextInt(5) + 1;
+        int randomNumber = random.nextInt(5);
         String cat1 = CategoryArray[0][randomNumber];
         return cat1;
     }
     public String Category2RandomValue(String[][]CategoryArray){
         Random random = new Random();
-        int randomNumber = random.nextInt(3) + 1;
+        int randomNumber = random.nextInt(3);
         String cat2 = CategoryArray[1][randomNumber];
         return cat2;
     }
     public String Category3RandomValue(String[][]CategoryArray){
         Random random = new Random();
-        int randomNumber = random.nextInt(6) + 1;
+        int randomNumber = random.nextInt(6);
         String cat3 = CategoryArray[2][randomNumber];
         return cat3;
     }
     public String Category4RandomValue(String[][]CategoryArray){
         Random random = new Random();
-        int randomNumber = random.nextInt(4) + 1;
+        int randomNumber = random.nextInt(4);
         String cat4 = CategoryArray[3][randomNumber];
         return cat4;
     }
     public String[] finalValues(){
-        String[]Student = new String[3];
+        String[]Student = new String[4];
         Student[0] = Category1RandomValue(CategoryArray);
         Student[1] = Category2RandomValue(CategoryArray);
         Student[2] = Category3RandomValue(CategoryArray);
         Student[3] = Category4RandomValue(CategoryArray);
         return Student;
-
     }
 
 }
