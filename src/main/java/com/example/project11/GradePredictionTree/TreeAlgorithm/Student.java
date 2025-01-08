6aefe531ca6dd487c55341351d6e07c38fa13441
Raul <r.parau@student.maastrichtuniversity.nl>
2025-01-08 11:33:49 +0100

    Area Chart and Bubble Chart with fxml files

---
+++ src/main/java/com/example/project11/GradePredictionTree/TreeAlgorithm/Student.java
@@ -0,0 +1,38 @@
+package com.example.project11.GradePredictionTree.TreeAlgorithm;
+
+import java.util.Random;
+
+public class Student {
+    private String[][] CategoryArray  = {{"full", "nothing", "medium","low", "high"},{"1 tau", "2 tau", "3 tau"},{"A","B","C","D","E","F"},{"0.1 Hz ", "0.5 Hz ", "1.0 Hz ","5.0 Hz "}};
+
+    public Student(String[][] CategoryArray){
+        this.CategoryArray = CategoryArray;
+    }
+
+    //sets Category 1 to a random value
+    public String Category1RandomValue(String[][]CategoryArray){
+        Random random = new Random();
+        int randomNumber = random.nextInt(5) + 1;
+        String cat1 = CategoryArray[0][randomNumber];
+        return cat1;
+    }
+    public String Category2RandomValue(String[][]CategoryArray){
+        Random random = new Random();
+        int randomNumber = random.nextInt(3) + 1;
+        String cat2 = CategoryArray[1][randomNumber];
+        return cat2;
+    }
+    public String Category3RandomValue(String[][]CategoryArray){
+        Random random = new Random();
+        int randomNumber = random.nextInt(6) + 1;
+        String cat3 = CategoryArray[2][randomNumber];
+        return cat3;
+    }
+    public String Category4RandomValue(String[][]CategoryArray){
+        Random random = new Random();
+        int randomNumber = random.nextInt(4) + 1;
+        String cat4 = CategoryArray[3][randomNumber];
+        return cat4;
+    }
+
+}
---
+++ src/main/java/com/example/project11/GradePredictionTree/TreeAlgorithm/TreeTester.java
@@ -0,0 +1,12 @@
+package com.example.project11.GradePredictionTree.TreeAlgorithm;
+
+public class TreeTester {
+    private int[]grades;
+
+    public TreeTester(int[]grades){
+        this.grades = grades;
+    }
+
+
+
+}
