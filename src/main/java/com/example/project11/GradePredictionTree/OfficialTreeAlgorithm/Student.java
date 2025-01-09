c6c97236bc03679898ea38463047bb80422081d3
marcos <m.vargasgarcia@student.maastrichtuniversity.nl>
2025-01-09 18:45:12 +0100

    Merge remote-tracking branch 'origin/Merge' into Merge
    # Conflicts:
    #	src/main/java/com/example/project11/ProjectInfo/STEP2/PredictPercentage.java

--- src/main/java/com/example/project11/GradePredictionTree/OfficialTreeAlgorithm/Student.java
+++ src/main/java/com/example/project11/GradePredictionTree/OfficialTreeAlgorithm/Student.java
@@ -1,46 +1,107 @@
 package com.example.project11.GradePredictionTree.OfficialTreeAlgorithm;
 
-import java.util.Random;
+import com.example.project11.ProjectInfo.loaders.StudentInfoLoader;
+
+import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
+import java.util.*;
+
 
 public class Student {
-    private double[][] CategoryArray;
 
-    public Student(double[][] CategoryArray){
+    private String[][] CategoryArrayInString ;
+    private List<List<String>> CategoryListInStringlist = new ArrayList<>();
 
-        this.CategoryArray = CategoryArray;
+    private int length2;
+    private String[][] studentInfoArray;
+    //private double[][] CategoryArray;
+
+    public Student(String[][] studentInfoArrayInput) throws FileNotFoundException {
+
+
+        this.studentInfoArray  = studentInfoArrayInput;
+
+        Set<String> uniqueProperty2 = new HashSet<>();
+
+        for (int i = 0 ; i<studentInfoArray.length ; i++) {
+            uniqueProperty2.add(studentInfoArray[i][2]); // Add to the set (ignores duplicates automatically)
+        }
+        List<String> listFor2 = new ArrayList<>(uniqueProperty2);
+
+        CategoryListInStringlist.add(List.of("full", "nothing", "medium", "low", "high"));
+        CategoryListInStringlist.add(listFor2); // Empty row
+        CategoryListInStringlist.add(List.of("1 tau", "2 tau", "3 tau"));
+        CategoryListInStringlist.add(List.of("A", "B", "C", "D", "E", "F"));
+        CategoryListInStringlist.add(List.of("0.1 Hz", "0.5 Hz", "1.0 Hz", "5.0 Hz"));
+
+
+
+        length2 = listFor2.size();
+        CategoryArrayInString = transform2DList(CategoryListInStringlist);
+
+
+       // this.CategoryArray = CategoryArray;
+    }
+
+    //Method to transform the list into a 2D array
+    public String[][] transform2DList(List<List<String>> CategoryListInString){
+        String[][] transformedArray = new String[CategoryListInString.size()][];
+
+        for(int i = 0; i < CategoryListInString.size(); i++){
+            transformedArray[i] = CategoryListInString.get(i).toArray(new String[0]);
+        }
+
+        return transformedArray;
     }
 
     //sets Category 1 to a random value
-    public double Category1RandomValue(){
+    public String Category1RandomValue(){
         Random random = new Random();
         int randomNumber = random.nextInt(5);
-        double cat1 = CategoryArray[0][randomNumber];
+        String cat1 = CategoryArrayInString[0][randomNumber];
         return cat1;
     }
-    public double Category2RandomValue(){
+
+    //sets Category 2 to a random value
+    public String Category2RandomValue(){
         Random random = new Random();
-        int randomNumber = random.nextInt(4);
-        double cat2 = CategoryArray[2][randomNumber];
+        int randomNumber = random.nextInt(length2);
+        String cat2 = CategoryArrayInString[1][randomNumber];
         return cat2;
     }
-    public double Category3RandomValue(){
+
+    //sets Category 3 to a random value
+    public String Category3RandomValue(){
+        Random random = new Random();
+        int randomNumber = random.nextInt(3);
+        String cat2 = CategoryArrayInString[2][randomNumber];
+        return cat2;
+    }
+
+    //sets Category 4 to a random value
+    public String Category4RandomValue(){
         Random random = new Random();
         int randomNumber = random.nextInt(6);
-        double cat3 = CategoryArray[3][randomNumber];
+        String cat3 = CategoryArrayInString[3][randomNumber];
         return cat3;
     }
-    public double Category4RandomValue(){
+
+    //sets Category 5 to a random value
+    public String Category5RandomValue(){
         Random random = new Random();
         int randomNumber = random.nextInt(4);
-        double cat4 = CategoryArray[4][randomNumber];
+        String cat4 = CategoryArrayInString[4][randomNumber];
         return cat4;
     }
-    public double[] finalValues(){
-        double[]Student = new double[4];
+
+    //Creates an array with all the final random values for each student
+    public String[] finalValues(){
+        String[]Student = new String[5];
         Student[0] = Category1RandomValue();
-        //Student[1] = Category2RandomValue();
+        Student[1] = Category2RandomValue();
         Student[2] = Category3RandomValue();
         Student[3] = Category4RandomValue();
+        Student[4] = Category5RandomValue();
         return Student;
     }
 
